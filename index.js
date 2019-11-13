187b4a13bea6abadb663ac3891cd104ea25d0bfc
Jasper Kim <yjikim0219@gmail.com>
2019-11-12 21:45:20 -0800

    Remove create/edit/delete

--- index.js
+++ index.js
@@ -3,48 +3,34 @@ var cacheManager = require('cache-manager');
 module.exports = {
 	init: function() {
 		this.cache = cacheManager.caching({
-			store: 'memory', max: process.env.CACHE_MAXSIZE || 1000, ttl: process.env.CACHE_TTL || 7776000/*90 days in seconds*/
+			store: 'memory', max: process.env.CACHE_MAXSIZE || 50, ttl: process.env.CACHE_TTL || 60/*seconds*/
 		});
 	},
 
 	requestReceived: function(req, res, next) {
 		const cache = this.cache;
-		cache.get(req.prerender.url, function (err, result) {	
-			const submitType = req.prerender.submitType;
-			
-			if(submitType === "reset") {
-				cache.reset();
-				console.log("Flushing cache!");
-				res.send(200, "Deleted Successfully");
-			}
-
-			if(!err && result) {
-				console.log("Cache Found!!");
-				if(submitType === "delete") {
-					req.prerender.cacheHit = true;
-					cache.del(req.prerender.url, function (err) {
-						console.log("Cached is deleted!");
-						res.send(200, "Deleted Successfully");
-					});
-				} else if(submitType === "create" || submitType === "edit") {
-					next();
-				} else {
-					req.prerender.cacheHit = true;
-					console.log("Cache gets returned!");
-					res.send(200, result);
-				}
-			} else if(!result && submitType === "delete") {
-				console.log('Cached is "ALREADY" deleted!');
-				res.send(404);
-			} else {
-				next();
-			}
-		});
-	},
-
-	beforeSend: function(req, res, next) {
 		const submitType = req.prerender.submitType;
 		
+		if(submitType === "reset") {
+			req.prerender.cacheHit = true;
+			cache.reset();
+			res.send(200, "Finish cache burster Successfully");
+			console.log('All caches are cleared!');
+		} else {
+			cache.get(req.prerender.url, function (err, result) {	
+				if(!err && result) {
+					req.prerender.cacheHit = true;
+					res.send(200, result);
+					console.log("Cache Found!!");
+					console.log("Cache gets returned!");
+				} else {
+					next();
+				}
+			});
+		}
+	},
+
+	beforeSend: function(req, res, next) {	
 		if (!req.prerender.cacheHit && req.prerender.statusCode == 200) {
 			this.cache.set(req.prerender.url, req.prerender.content);
 			console.log("Cache is created!");
