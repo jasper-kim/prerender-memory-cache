061d009fa7c344ec99eb988c8f5d593bb7d0b614
Jasper Kim <yjikim0219@gmail.com>
2019-11-26 20:24:47 -0800

    Remove reset function

--- index.js
+++ index.js
@@ -8,32 +8,19 @@ module.exports = {
 	},
 
 	requestReceived: function(req, res, next) {
-		const cache = this.cache;
-		const submitType = req.prerender.submitType;
-		
-		if(submitType === "reset") {
-			req.prerender.cacheHit = true;
-			cache.reset();
-			res.send(200, "Finish cache burster Successfully");
-			console.log('All caches are cleared!');
-		} else {
-			cache.get(req.prerender.url, function (err, result) {	
-				if(!err && result) {
-					req.prerender.cacheHit = true;
-					res.send(200, result);
-					console.log("Cache Found!!");
-					console.log("Cache gets returned!");
-				} else {
-					next();
-				}
-			});
-		}
+		this.cache.get(req.prerender.url, function (err, result) {
+			if (!err && result) {
+				req.prerender.cacheHit = true;
+				res.send(200, result);
+			} else {
+				next();
+			}
+		});
 	},
 
-	beforeSend: function(req, res, next) {	
+	beforeSend: function(req, res, next) {
 		if (!req.prerender.cacheHit && req.prerender.statusCode == 200) {
 			this.cache.set(req.prerender.url, req.prerender.content);
-			console.log("Cache is created!");
 		}
 		next();
 	}
